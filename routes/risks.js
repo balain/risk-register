07a0026d7da397cd1e67b1b7db393fb656215a8d
John D. Lewis <balain@users.noreply.github.com>
2020-11-16 23:30:55 -0500

    Updated views and placeholder edit form

--- routes/risks.js
+++ routes/risks.js
@@ -8,8 +8,42 @@ router.get('/', async function (req, res, next) {
   res.render('risks', { title: 'Risks', risks: risks })
 })
 
-router.post('/', function (req, res, next) {
-  res.write('Not yet implemented')
+router.get('/view', async function (req, res, next) {
+  const risks = await db.Risk.findAll()
+  console.log(risks)
+  res.render('risks', { title: 'Risks', risks: risks })
+})
+
+router.post('/update', async function(req, res, next) {
+  res.write('Update not yet implemented')
+})
+
+router.get('/edit/:id', async function (req, res, next) {
+  const risk = await db.Risk.findOne({ where: { id: req.params.id } })
+  try {
+    if (risk) {
+      res.render('risk-edit', { title: `Risk ${risk.dataValues.id}`, risk: risk.dataValues })
+    } else {
+      res.send(`No risk found with ID ${req.params.id}`)
+    }
+  } catch (err) {
+    console.log(err)
+    res.write(`error: ${err}`)
+  }
+})
+
+router.get('/view/:id', async function (req, res, next) {
+  const risk = await db.Risk.findOne({ where: { id: req.params.id } })
+  try {
+    if (risk) {
+      res.render('risk', { title: `Risk ${risk.dataValues.id}`, risk: risk.dataValues })
+    } else {
+      res.send(`No risk found with ID ${req.params.id}`)
+    }
+  } catch (err) {
+    console.log(err)
+    res.write(`error: ${err}`)
+  }
 })
 
 module.exports = router
